687bd6a31dfc455ac60fb8f5ef83c7034f195ead
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2025-08-18 00:45:56 +0900

    Version 0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,25 +16,14 @@ module.exports = {
     },
 
     colors: {
-      transparent: "transparent",
-      primary: "#072344",
-      secondary: "#0000a1",
-      "green-light": "#cceeec",
-      green: "#007c85",
-      "green-dark": "#065a68",
-      "blue-light": "#b3d6f1",
-      blue: "#0074d1",
-      "blue-dark": "#072344",
-      black: "#000000",
-      white: "#ffffff",
-      "yellow-lighter": "#f6e8c6",
-      "yellow-light": "#f8edd0",
-      yellow: "#f4d06f",
-      "yellow-dark": "#daa512",
-      "grey-lightest": "#eff0f3",
-      "grey-lighter": "#eceef1",
-      "grey-light": "#ccd7e0",
-      grey: "#adb6c4",
+       light: {
+          primary: "#072344",
+          secondary: "#0000a1",
+       },
+       dark: {
+            primary: "#072344",
+            secondary: "#0000a1",
+       }
     },
     border: {
       DEFAULT: "1px",
